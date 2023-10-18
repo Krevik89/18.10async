eaa858b1265519bef324f97d655a8e7b1e402389
PingWin <vitya.staritsin@yandex.ru>
2023-10-18 20:59:29 +0300

    Добавьте файлы проекта.

---
+++ 18.10async/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _18._10async
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();        
+        }
+
+
+        string SlowMethod(CancellationToken token)
+        {
+            Thread.Sleep(5000);
+            token.ThrowIfCancellationRequested();
+            return "Жди";
+        }
+
+        async private Task GetDataAsync(string filePath)
+        {         
+            byte[] data = null;
+            using (FileStream fs = File.Open(filePath, FileMode.Open))
+            {
+                data = new byte[fs.Length];
+                await fs.ReadAsync(data, 0, data.Length);
+            }
+            Tbox.Text += Encoding.UTF8.GetString(data);
+        }
+
+        private Task<string> SlowMethofAsync(CancellationToken token)
+        {
+            return Task.Run<string>(() => { return SlowMethod(token); });
+        }
+
+        async private  void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var cts = new CancellationTokenSource();
+                cts.CancelAfter(TimeSpan.FromSeconds(3));
+
+                Tbox.Text = "before async code\n";
+                //await GetDataAsync(@"C:\Users\РПО\Desktop\Работа с файлами.txt");
+                Tbox.Text += await SlowMethofAsync(cts.Token);
+                //Tbox.Text=SlowMethod();
+                Tbox.Text = "\nafter async code\n";
+            }catch(OperationCanceledException es)
+            {
+                MessageBox.Show(es.Message);
+            }
+            
+
+            //Tbox.Text=SlowMethod();
+            //await GetDataAsync(@"C:\Users\РПО\Desktop\Работа с файлами.txt");
+
+            //CancellationTokenSource; отмена операции в др методе
+
+        }
+    }
+}
+// async await;
+// int r = await MethodAsync();
+// await MethodAsync(); 
+// ecли void Task t = MethodAsync();
+// await t;
+
+/*private async void Foo()
+        {
+            InitializeComponent();
+            //async await;
+            int r = await MethodAsync(); //ecли void
+        }
+
+        async Task<int> MethodAsync()
+        {
+            int result=0;
+            //code
+            //result  object <T>;
+            return result;
+        }
+
+        async Task MethodAsync2() //async void или async Task
+        {
+            //code
+        }
+*/
---
+++ Fabric_await_sql/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Fabric_await_sql
+{
+    
+    public partial class MainWindow : Window
+    {
+        DbConnection conn=null;
+        DbProviderFactory fact = null;
+        string ConnectionString = "";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private async void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            conn.ConnectionString = ConnectionString;
+            await conn.OpenAsync();
+
+            DbCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "waitfor delay '00:00:04';select * from dbo.Products";
+            cmd.CommandText += textbox1.Text;
+
+            DataTable table = new DataTable();
+            using (DbDataReader reader = await cmd.ExecuteReaderAsync())
+            {
+                // do while...
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            fact = DbProviderFactories.GetFactory("System.Data.SqlClient");
+            conn = fact.CreateConnection();
+            ConnectionString = GetConectionstring("System.Data.SqlClient");
+        }
+        private string GetConectionstring(string connName)
+        {
+            return ConfigurationManager.ConnectionStrings[ connName]; 
+        }
+
+    }
+}
